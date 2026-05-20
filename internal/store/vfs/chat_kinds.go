38f0c235f4099bc70d25a0e045a3cef953bc02a0
Rui Carmo <rui.carmo@gmail.com>
2026-05-20 21:03:47 +0100

    refactor(store): move chat vfs kind constants into vfs subpackage

---
+++ internal/store/vfs/chat_kinds.go
@@ -0,0 +1,14 @@
+package vfs
+
+const (
+	KindChatReadme        = "chat_readme"
+	KindChatSessions      = "chat_sessions"
+	KindChatSessionsIndex = "chat_sessions_index"
+	KindChatSession       = "chat_session"
+	KindChatMessages      = "chat_messages"
+	KindChatMessagesIndex = "chat_messages_index"
+	KindChatMessage       = "chat_message"
+	KindChatTurns         = "chat_turns"
+	KindChatTurnsIndex    = "chat_turns_index"
+	KindChatTurn          = "chat_turn"
+)
